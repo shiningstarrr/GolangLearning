a4d02f57c5e0da721df36c2d8b2e7611f4d6c506
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-01-03 01:10:02 -0500

    you are doing great

--- PracticeCode/practice.go
+++ PracticeCode/practice.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"sort"
 )
 
 func main() {
-	x := []string{"hello", "world", "prabably", "not"}
-	v, err := json.Marshal(x)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(v))
+	xi := []int{5, 8, 2, 43, 293, 88, 66, 51, 78}
+	xs := []string{"random", "rainbor", "delight", "in", "tornnaod", "summer", "under"}
+
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println("after sort:", xi)
+
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println("after sort:", xs)
+
+	x := sort.Search(len(xi), func(i int) bool { return xi[i] == 88 })
+	fmt.Println("x equals to", x)
+
 }
