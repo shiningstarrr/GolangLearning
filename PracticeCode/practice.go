a8d7344bb81590786b16f7d3ff8a2f004f39d412
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-01-03 07:34:27 -0500

    you are doing great

--- PracticeCode/practice.go
+++ PracticeCode/practice.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-	xi := []int{5, 8, 2, 43, 293, 88, 66, 51, 78}
-	xs := []string{"random", "rainbor", "delight", "in", "tornnaod", "summer", "under"}
 
-	fmt.Println(xi)
-	sort.Ints(xi)
-	fmt.Println("after sort:", xi)
-
-	fmt.Println(xs)
-	sort.Strings(xs)
-	fmt.Println("after sort:", xs)
-
-	x := sort.Search(len(xi), func(i int) bool { return xi[i] == 88 })
-	fmt.Println("x equals to", x)
+	people := []struct {
+		Name string
+		Age  int
+	}{
+		{"Alice", 25},
+		{"Zlizabeth", 75},
+		{"Alice", 75},
+		{"Gob", 75},
+		{"Alice", 75},
+		{"Fob", 25},
+		{"Colin", 25},
+		{"Elizabeth", 25},
+	}
+	fmt.Println("before sort: ", people)
+	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
+	fmt.Println("after sort: ", people)
 
 }
