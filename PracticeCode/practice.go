606a78388f5294569079d89689ae972a71139b17
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-01-05 22:40:18 -0500

    package sync

--- PracticeCode/practice.go
+++ PracticeCode/practice.go
@@ -2,26 +2,50 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	"runtime"
+	"sync"
 )
 
+var wg sync.WaitGroup
+var mu sync.Mutex
+
 func main() {
-
-	people := []struct {
-		Name string
-		Age  int
-	}{
-		{"Alice", 25},
-		{"Zlizabeth", 75},
-		{"Alice", 75},
-		{"Gob", 75},
-		{"Alice", 75},
-		{"Fob", 25},
-		{"Colin", 25},
-		{"Elizabeth", 25},
+	counter := 0
+	fmt.Println("goroutine times: ", runtime.NumGoroutine())
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
 	}
-	fmt.Println("before sort: ", people)
-	sort.Slice(people, func(i, j int) bool { return people[i].Name < people[j].Name })
-	fmt.Println("after sort: ", people)
+	wg.Wait()
+	fmt.Println("goroutine times: ", runtime.NumGoroutine())
+	fmt.Println("count = ", counter)
 
+}
+
+func foo() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("bar:", i)
+	}
+}
+
+func grr() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("grr:", i)
+	}
+	fmt.Println("done_2")
 }
