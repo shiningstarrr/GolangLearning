c0c7251c436638e200766c5c4d741ea236704c64
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-01-07 01:09:01 -0500

    start

--- PracticeCode/practice.go
+++ PracticeCode/practice.go
@@ -3,31 +3,13 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"sync"
-	"sync/atomic"
 )
-
-var wg sync.WaitGroup
 
 func main() {
 	fmt.Println("start")
-	fmt.Println("goroutines: ", runtime.NumGoroutine())
-	var counter int64
-	wg.Add(100)
-
-	for i := 0; i < 100; i++ {
-		go func() {
-			atomic.AddInt64(&counter, 1)
-
-			//runtime.Gosched()
-			fmt.Println(atomic.LoadInt64(&counter))
-
-			wg.Done()
-			//fmt.Println("goroutines: ", runtime.NumGoroutine())
-		}()
-	}
-	wg.Wait()
-	fmt.Println("count: ", counter)
-	fmt.Println("goroutines: ", runtime.NumGoroutine())
+	const os = runtime.GOOS
+	const ar = runtime.GOARCH
+	fmt.Println("os:", os)
+	fmt.Println("arch:", ar)
 
 }
