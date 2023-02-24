2ad072a95c0f8e2a27657293250f0af0b42298a9
wangj13 <115693516+shiningstarrr@users.noreply.github.com>
2023-02-24 19:05:02 +0800

    godoc test

--- PracticeCode/main.go
+++ PracticeCode/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	mymaththree "GolangLearning/PracticeCode/myMathThree"
+	//mymaththree "GolangLearning/PracticeCode/myMathThree"
+
 	"fmt"
+
+	"github.com/shiningstarrr/GolangLearning/PracticeCode/mymaththree"
 )
 
 func main() {
-	fmt.Println("2 + 3 =", mymaththree.Sum(2, 3))
-	fmt.Println("4 + 7 =", mymaththree.Sum(4, 7))
-	fmt.Println("5 + 9 =", mymaththree.Sum(5, 9))
+	fmt.Println("2 + 3 =", mymathThree.Sum(2, 3))
+	fmt.Println("4 + 7 =", myMathThree.Sum(4, 7))
+	fmt.Println("5 + 9 =", myMathThree.Sum(5, 9))
 }
